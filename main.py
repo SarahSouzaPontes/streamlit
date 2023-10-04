e1655b41c6d179c68cb20506fa83dc0cdeccd522
Sarah Souza Pontes <90557782+SarahSouzaPontes@users.noreply.github.com>
2023-10-04 18:45:02 +0000

    Upgrad

--- main.py
+++ main.py
@@ -1,29 +1,7 @@
-#pip install pandas numpy streamlit seaborn
-
-import io
-
-data = io.BytesIO(uploaded[r'C:\Users\Sarah.Pontes\Downloads\Call.csv'])
-df = pd.read_csv(data)
-
-#!pip install requests
-
+import streamlit as st
 import pandas as pd
 import numpy as np
-import streamlit as st
-import seaborn as sns
-#import requests
 
-st.title('Dashboard Cliente')
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
-#convertendo colunas
-#df['Answer Rate'] = df['Answer Rate'].str.rstrip("%").astype(float)/100
-
-# Remove '%' symbol, convert to float, and handle non-numeric values
-df['Answer Rate'] = pd.to_numeric(df['Answer Rate'].str.rstrip('%'), errors='coerce') / 100
-
-col1, col2, col3 = st.columns(3)
-col1.metric("Incoming Calls AVG", round(incoming_avg))
-col2.metric("Answered Calls AVG", round(ansred_avg))
-col3.metric("Answer Rate AVG", round(ans_avg))
-
-st.line_chart(df[['Incoming Calls', 'Answered Calls']])
+st.area_chart(chart_data)
